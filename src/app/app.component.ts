53a22c0d9a40762a7a5d53b1be993378a09bfd7d
Fihoca02 <Fihoca02@SINENSIA.EDU>
2019-12-05 14:26:12 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng/api/selectitem';
+
+interface Empresa {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'showcasePrimeNg';
+export class AppComponent implements OnInit{
+    
+  coches:any[]=[];
+  date1:Date = new Date();
+  selectedCities:any[]=[];
+  val1:number=5;
+  val2:number=0;
+  empresas1:SelectItem[];
+  empresas2:Empresa[];
+  selectedEmpresa1:Empresa;
+  selectedEmpresa2:Empresa;
+  display: boolean = false;
+
+  constructor(){
+    this.empresas1=[
+      {label:"Select a company", value:""}, 
+      {label:"ADEPSA", value:{id:1, name: 'ADEPSA', code: 'AD'}}, 
+      {label:"CIRCOSA", value:{id:2, name: 'CIRCOSA', code: 'CI'}}
+     ];
+
+    this.empresas2 = [
+      {name: 'ADEPSA', code: 'AD'},
+      {name: 'CIRCOSA', code: 'CI'}
+  ];
+  }
+
+  ngOnInit(): void {
+   this.coches.push({marca:"SEAT", modelo:"IBIZA"});
+   this.coches.push({marca:"SEAT", modelo:"Tarraco"});
+   this.coches.push({marca:"SEAT", modelo:"León"});
+  
+   this.date1 = new Date(23-12-1912);
+  
+   
+
+   }
+   showDialog() {
+    this.display = true;
+}
+
+    
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//Dependencias de PrimeNG
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+import {CardModule} from 'primeng/card';
+import {CalendarModule} from 'primeng/calendar';
+import {InputTextModule} from 'primeng/inputtext';
+import {CheckboxModule} from 'primeng/checkbox';
+import {RatingModule} from 'primeng/rating';
+import {SpinnerModule} from 'primeng/spinner';
+import {DropdownModule} from 'primeng/dropdown';
+import {DialogModule} from 'primeng/dialog';
+import {TooltipModule} from 'primeng/tooltip';
+import {MenuModule} from 'primeng/menu';
+import {MenuItem} from 'primeng/api';
 
 @NgModule({
   declarations: [
@@ -10,7 +27,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    TableModule,
+    CardModule,
+    CalendarModule,
+    FormsModule,
+    InputTextModule,
+    CheckboxModule,
+    RatingModule,
+    SpinnerModule,
+    DropdownModule,
+    DialogModule,
+    TooltipModule,
+    MenuModule,
+    MenuItem
+
   ],
   providers: [],
   bootstrap: [AppComponent]
